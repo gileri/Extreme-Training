b58adf2f8b0c1ae95afef0d1ef76fa00017308b3
Éric Gillet <eric.gillet+gh@linuxw.info>
2015-01-08 15:19:01 +0100

    Start result view

--- war/js/search.js
+++ war/js/search.js
@@ -10,12 +10,38 @@ function getUrlParameter(sParam)
             return sParameterName[1];
         }
     }
-}          
-
-var loaded = function() {
-	var search =getUrlParameter("searchBar");
-	$.post("/search", {searchBar: search}, function(data) {
-		$("#training").html(data);
-	})
-	
 }
+
+var f = function() {
+	var search =getUrlParameter("searchBar");
+	$("#searchBar").val(search);
+	$.post("/search", {searchBar: search}, function(data) {
+		var obj = $.parseJSON(data);
+		
+		var numTrainings = 0;
+		obj.trainings.forEach(function(e) {
+			var duration = 0;
+			e.forEach(function(f) {
+				duration = duration + f.duration;
+			});
+			var dupe = $("#training_template")
+			.clone()
+			.appendTo("#training_section");
+			
+			dupe.find(".name").html(e.name);
+			dupe.find(".duration").html(duration);
+			dupe.removeClass("hidden");
+		});
+		
+		obj.exercise.forEach(function(e) {
+			var dupe = $("#exercise_template")
+			.clone()
+			.appendTo("#exercise_section");
+			
+			dupe.find(".name").html(e.name);
+			dupe.find(".duration").html(e.duration);
+			dupe.removeClass("hidden");
+		});
+	})
+}
+loaded.push(f);
